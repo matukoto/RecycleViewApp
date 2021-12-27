47a354f5492fce84b3d1ce7faa91e5be5fafd394
DESKTOP-4V10F99\matsumoto keisuke <keisuke.suke.san@gmail.com>
2021-12-27 23:03:59 +0900

    #1 recyclerView を作成しました

--- app/src/main/java/com/example/recycleviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.recycleviewapp
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.recycleviewapp.adapter.ItemAdapter
+import com.example.recycleviewapp.data.Datasource
+import com.example.recycleviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataset = Datasource().loadAffirmations()
+
+        val recycleView = binding.recyclerView
+        recycleView.adapter = ItemAdapter(this, myDataset)
+
+        // recycleView.setHasFixedSize(true)
+
     }
 }
---
+++ app/src/main/java/com/example/recycleviewapp/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.recycleviewapp.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
