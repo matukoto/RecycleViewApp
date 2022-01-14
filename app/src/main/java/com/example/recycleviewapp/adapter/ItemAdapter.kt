f13957cf8942a46e62b048c59c013adb91333cd6
DESKTOP-4V10F99\matsumoto keisuke <keisuke.suke.san@gmail.com>
2022-01-14 16:15:44 +0900

    RecyclerView に画像を追加しました

--- app/src/main/java/com/example/recycleviewapp/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recycleviewapp/adapter/ItemAdapter.kt
@@ -3,7 +3,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recycleviewapp.R
 import com.example.recycleviewapp.model.Affirmation
@@ -17,6 +19,7 @@ class ItemAdapter(
 
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -29,6 +32,7 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int = dataset.size
